2f2975fbe5c0687f23ed82c6f1258a02c0348866
Ann Castro <anncasstro@gmail.com>
2024-07-31 18:15:24 -0500

    Add ejercicio ADSO para iniciar el proyecto

---
+++ index.js
@@ -0,0 +1,5 @@
+let basetriangulo = parseInt(prompt("Ingrese la base del triangulo "));
+let alturatriangulo = parseInt(prompt("Ingrese la altura del tiangulo"));
+let resultado = basetriangulo + alturatriangulo;
+console.log(" Su resultado es " + resultado);
+document.write("Su resultado es " + resultado);
